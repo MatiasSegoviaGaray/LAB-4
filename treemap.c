ad8a7e4c944b101c4925d0ffa66e17533ff0e0f7
Your Name <you@example.com>
2026-04-30 20:49:22 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -64,11 +64,14 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
     
     while(key != tree -> current -> pair -> key){
         TreeMap *aux = tree -> current -> pair -> key;
+
+        if(aux == key) return tree -> current -> pair;
+        
         if(tree -> lower_than(aux, key) == 1) tree -> current = tree -> current -> right;
         else tree -> current = tree -> current -> left;
+        
         if(tree -> current -> right == NULL && tree -> current -> left == NULL) return NULL;
     }
-    return tree -> current -> pair;
 }
 
 // 3. Implemente la función void insertTreeMap(TreeMap * tree, void* key, void * value). 
